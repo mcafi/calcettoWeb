b7a14ae5a48fbdda0eb6ba4127f14b3e518f8453
mcafi <matteo.carniglia@gmail.com>
2021-04-06 23:11:44 +0200

    login improvements

--- src/router/index.js
+++ src/router/index.js
@@ -1,53 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-//import { auth } from '../firebase'
-import firebase from "firebase/app"
+import { store } from "../store/index"
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-    meta: {
-      requiresAuth: true
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/login',
+        name: "Login",
+        component: Login
+    },
+    {
+        path: "/profile",
+        name: "Profile",
+        component: () =>
+            import ("../views/Profile.vue"),
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: "/settings",
+        name: "Settings",
+        component: () =>
+            import ("../views/Settings.vue"),
+        meta: {
+            requiresAuth: true
+        }
     }
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/login',
-    name: "Login",
-    component: Login
-  },
-  {
-    path: "/profile",
-    name: "Profile",
-    component: () => import("../views/Profile.vue")
-  }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 router.beforeEach((to, from, next) => {
-  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
-  firebase.auth().onAuthStateChanged(user => {
-    if (!user && requiresAuth){
-      console.log("XD")
-      next("/login")
+    const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+    if (!Object.keys(store.getters.getUser).length && requiresAuth) {
+        next("/login")
     } else {
-      next()
+        next()
     }
-  });
 })
 
 export default router
